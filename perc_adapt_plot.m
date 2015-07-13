512519318790afb45ec0305ce85dd4cde0bfe2b2
rlinus <linus.ruettimann@gmail.com>
2015-07-13 19:14:17 +0200

    c

--- perc_adapt_plot.m
+++ perc_adapt_plot.m
@@ -6,7 +6,6 @@ for i=1:data.n_trials
     b = round((data.voice_onset_ms(i)+300)/1000*data.Fs/data.frameSize);
     e = round(0.1*data.Fs/data.frameSize);
     p = data.detected_pitch{i}(b:end-e);
-    %length(p)
     outdata.produced_freq_mean(i) = mean(p(p~=0));
 end
 
@@ -35,11 +34,56 @@ figure; hold on;
 x1 = 1:sum(data.kind_of_trials==2);
 plot(x1,1200*log2(data.ref_freq_sqs(data.kind_of_trials==2)/data.ref_freq));
 plot(x1,1200*log2(outdata.produced_freq_mean(data.kind_of_trials==2)/data.ref_freq));
+plot(x1,data.pitch_level_sqs_cents(data.kind_of_trials==2));
 title('motor catch trials: absolute performance');
 
 
 %motor catch trial
-% figure; hold on;
-% x1 = 1:sum(data.kind_of_trials==2);
+x1 = 1:sum(data.kind_of_trials==2);
+x2=x(data.kind_of_trials==2);
+prevs = nan(1,length(x2));
+for i=1:length(x2)
+    for j=x2(i)-1:-1:1
+        if data.kind_of_trials(j)==1
+            prevs(i)=j;
+            break;
+        end
+    end
+end
+x3=x1(~isnan(prevs));
+prevs=prevs(~isnan(prevs));
+
+
+figure; hold on;
+
+plot(x1,1200*log2(outdata.produced_freq_mean(data.kind_of_trials==2)./data.ref_freq_sqs(data.kind_of_trials==2)));
+plot(x1,data.pitch_level_sqs_cents(data.kind_of_trials==2));
+plot(x1,data.perceived_produced_pitch(data.kind_of_trials==2));
+plot(x3,data.pitch_level_var_sqs_cents(prevs));
+title('motor catch trials');
+
+%perceptual probe trials
+x1 = 1:sum(data.kind_of_trials==3);
+x2=x(data.kind_of_trials==3);
+prevs = nan(1,length(x2));
+for i=1:length(x2)
+    for j=x2(i)-1:-1:1
+        if data.kind_of_trials(j)==1
+            prevs(i)=j;
+            break;
+        end
+    end
+end
+x3=x1(~isnan(prevs));
+prevs=prevs(~isnan(prevs));
+
+
+figure; hold on;
+
+plot(x1,data.pitch_level_sqs_cents(data.kind_of_trials==3));
+plot(x1,data.perceived_produced_pitch(data.kind_of_trials==3));
+plot(x3,data.pitch_level_var_sqs_cents(prevs));
+title('perceptual probe trials');
+
 end
 
